df4c22945dc9623b94bc82abd79bd9299a281485
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-10-29 01:32:53 +0200

    Update color options based on select value

--- js/script.js
+++ js/script.js
@@ -7,5 +7,20 @@ const otherJob = document.getElementById("other-job-role");
 const selectJob = document.getElementById("title");
 otherJob.style.display = "none";
 selectJob.addEventListener("change", () => {
+  // Display or hide other job field
   otherJob.style.display = selectJob.value === "other" ? "inherit" : "none";
+});
+
+// T-shirt selection
+const selectColor = document.getElementById("color");
+const selectDesign = document.getElementById("design");
+selectColor.setAttribute("disabled", "");
+selectDesign.addEventListener("change", () => {
+  // Enable color selection
+  selectColor.removeAttribute("disabled");
+
+  // Update color selection options based on design select value
+  [...document.querySelectorAll("option[data-theme]")].forEach(option => {
+    option.dataset.theme !== selectDesign.value ? option.setAttribute("hidden", "") : option.removeAttribute("hidden")
+  });
 });
